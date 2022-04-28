690a6ff0cff6119e4542120ad1a42f99f1807a89
unknown <ngocanha85@gmail.com>
2022-04-28 08:33:27 +0700

    Sua lan 3

--- script/breed.js
+++ script/breed.js
@@ -1,6 +1,31 @@
 let sideBar = document.getElementById("sidebar")
+let breedInput = document.getElementById("input-breed")
+let breedTypeInput = document.getElementById("input-type")
 
 // side bar toggle class "active"
 function toggleSideBar() {
     sideBar.classList.toggle("active")
+}
+
+const breedArr = []
+
+function formBreed() {
+    const data = breedInput.value
+    breedArr.push(data)
+    console.log(breedArr);
+    clearBreedForm()
+}
+
+
+function validateBreedForm(breed) {
+    if (!breed) {
+        alert("Please type breed!")
+        return false
+    }
+    return true
+}
+
+function clearBreedForm() {
+    breedInput.value = ""
+    breedTypeInput.value = "Select Type"
 }
